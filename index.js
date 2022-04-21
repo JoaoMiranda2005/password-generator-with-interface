93201398c9882af4c790e2dc7fbd8d103583e4ef
João Miranda <88510677+JoaoMiranda2005@users.noreply.github.com>
2022-04-21 21:05:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getPassword() {
     var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+";
-    var passwordLength = 16;
+    var passwordLength = 16; //Basta Alterar este numero para mudar o nr de caracteres da password
     var pasword = "";
 
     for (var i = 0; i < passwordLength; i++) {
